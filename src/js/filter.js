b598639a0c5549693ba40ac37b5f4974e712f350
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-19 22:37:14 +0200

    f

--- src/js/filter.js
+++ src/js/filter.js
@@ -4,17 +4,17 @@ import { APLCategories } from "./APIFoodBoutique";
 export const refsfilt = {
   filtercard: document.querySelector('.filter-cards'),
 };
-try {
-  const categResult = await APLCategories(); 
-categResult.push('Show all');
-const refs = {
-  categor: document.querySelector('.filter-categories'),
-};
-const markUpCategories = categResult.map(data => `<option value="${data}">${data}</option>`).join('');
-refs.categor.innerHTML += markUpCategories; 
-} catch (error) {
-  console.log(error);
-}
+// try {
+//   const categResult = await APLCategories(); 
+// categResult.push('Show all');
+// const refs = {
+//   categor: document.querySelector('.filter-categories'),
+// };
+// const markUpCategories = categResult.map(data => `<option value="${data}">${data}</option>`).join('');
+// refs.categor.innerHTML += markUpCategories; 
+// } catch (error) {
+//   console.log(error);
+// }
 
 // const seacrhresult = await APLProduct('', '', '', '', '', 1);
 // console.log(seacrhresult);
