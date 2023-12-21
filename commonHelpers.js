07f2c79db6efc06c458aaebccd9744be09a4afc8
xVenomUA <117669302+xVenomUA@users.noreply.github.com>
2023-12-21 15:54:46 +0000

    Deploying to gh-pages from @ xVenomUA/project-js-dreamteam@f066706dda5cccf2ded077144d7fecfe6280b175 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{a}from"./assets/vendor-a61d8330.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&c(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function c(e){if(e.ep)return;e.ep=!0;const s=o(e);fetch(e.href,s)}})();const l="https://food-boutique.b.goit.study/api";async function f(){try{return(await a.get(`${l}/products/categories`)).data}catch(t){console.log(t)}}async function d(t,r,o,c,e,s){const i=new URLSearchParams({q:t||!1,category:r||"",byABC:o||!1,byPrice:c||!1,byPopularity:e||!1,page:s,limit:6});return(await a.get(`${l}/products?${i.toString()}`)).data}function p(t){const r=t.map(({category:o,img:c,name:e,size:s,popularity:i,price:n})=>`<div class="filt-div-card">
+import{a as n}from"./assets/vendor-a61d8330.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&c(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function c(e){if(e.ep)return;e.ep=!0;const s=o(e);fetch(e.href,s)}})();const l="https://food-boutique.b.goit.study/api";async function f(){try{return(await n.get(`${l}/products/categories`)).data}catch(t){console.log(t)}}async function d(t,r,o,c,e,s){const i=new URLSearchParams({q:t||!1,category:r||"",byABC:o||!1,byPrice:c||!1,byPopularity:e||!1,page:s,limit:6});return(await n.get(`${l}/products?${i.toString()}`)).data}const p="/project-js-dreamteam/assets/icon-2165bba5.svg";function g(t){const r=t.map(({category:o,img:c,name:e,size:s,popularity:i,price:a})=>`<div class="filt-div-card">
       <div class="filt-div-img">
   <img class="filt-img" src="${c}" alt="${e}" width="140">
   </div>
@@ -9,13 +9,13 @@ import{a}from"./assets/vendor-a61d8330.js";(function(){const r=document.createEl
     <p class="filt-text">Popularity: <span class="filt-span">${i}</span></p>
   </div>
   <div class="filt-div-price"> 
-    <h2 class="filt-h-price">${n}</h2>
+    <h2 class="filt-h-price">${a}</h2>
     <button class="filt-btn-card" type="submit">
       <svg class="filt-card-icon" height="18" width="18" >
-          <use href="./img/icon.svg#icon-shop"></use>
+          <use href="${p}#icon-shop"></use>
         </svg>
     </button>
   </div>
 </div>
-`).join("");u.filtercard.innerHTML=r}async function g(){try{const t=await f();t.push("Show all");const r=t.map(o=>`<option value="${o}">${o}</option>`).join("");u.categor.innerHTML+=r}catch(t){console.log(t)}}async function y(){try{const r=(await d("","","","","",1)).results;console.log(r),p(r)}catch(t){console.log(t)}}g();y();const u={categor:document.querySelector(".filter-categories"),filtercard:document.querySelector(".filter-cards")};
+`).join("");u.filtercard.innerHTML=r}async function y(){try{const t=await f();t.push("Show all");const r=t.map(o=>`<option value="${o}">${o}</option>`).join("");u.categor.innerHTML+=r}catch(t){console.log(t)}}async function h(){try{const r=(await d("","","","","",1)).results;console.log(r),g(r)}catch(t){console.log(t)}}y();h();const u={categor:document.querySelector(".filter-categories"),filtercard:document.querySelector(".filter-cards")};
 //# sourceMappingURL=commonHelpers.js.map
