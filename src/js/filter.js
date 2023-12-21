ee3d21ab9fd53eecb6305015c443d5103a212561
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-21 17:36:23 +0200

    FIx

--- src/js/filter.js
+++ src/js/filter.js
@@ -11,10 +11,10 @@ import { FilterMarkUp } from "./FilterMarkUp";
 // ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴
 async function GetCategories(){
 try {
-  const categResult = await APICategories(); 
-categResult.push('Show all');
-const markUpCategories = categResult.map(data => `<option value="${data}">${data}</option>`).join('');
-refs.categor.innerHTML += markUpCategories; 
+  const seacrhresult = await APIProductSearch('', '', '', '', '', 1);
+  const results = seacrhresult.results;
+  console.log(results);
+  FilterMarkUp(results); 
 } catch (error) {
   console.log(error);
   }
@@ -25,16 +25,16 @@ refs.categor.innerHTML += markUpCategories;
 // РЕНДЕР КАРТОК В СЕЛЕКТІ
 // ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴
 // ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴
-async function GetCards() { 
-  try {
-    const seacrhresult = await APIProductSearch('', '', '', '', '', 1);
-    const results = seacrhresult.results;
-    console.log(results);
-    FilterMarkUp(results); 
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function GetCards() { 
+//   try {  
+//     const seacrhresult = await APIProductSearch('', '', '', '', '', 1);
+//     const results = seacrhresult.results;
+//     console.log(results);
+//     FilterMarkUp(results); 
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 GetCategories();
-GetCards();
+// GetCards();
