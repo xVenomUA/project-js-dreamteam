c0def61dbe862dcd7350a6da33f6e2fd017307b7
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-21 17:09:37 +0200

    f

--- src/js/APIFoodBoutique.js
+++ src/js/APIFoodBoutique.js
@@ -1,8 +1,13 @@
 import axios from "axios"
 
+const BASE_URL = 'https://food-boutique.b.goit.study/api';
+
 export async function APLCategories() { 
-    const response = await axios.get(
-      `https://food-boutique.b.goit.study/api/products/categories`
-    );
-    return response.data; 
+  try {
+     const response = await axios.get(
+       `${BASE_URL}/products/categories`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/js/filter.js
+++ src/js/filter.js
@@ -1,20 +1,27 @@
+
 import { APLCategories } from "./APIFoodBoutique";
 // import { APLProduct } from "./APIFoodSearch";
-// import { FilterMarkUp } from "./FilterMarkUp";
 export const refsfilt = {
   filtercard: document.querySelector('.filter-cards'),
 };
-// try {
-//   const categResult = await APLCategories(); 
-// categResult.push('Show all');
-// const refs = {
-//   categor: document.querySelector('.filter-categories'),
-// };
-// const markUpCategories = categResult.map(data => `<option value="${data}">${data}</option>`).join('');
-// refs.categor.innerHTML += markUpCategories; 
-// } catch (error) {
-//   console.log(error);
+const refs = {
+  categor: document.querySelector('.filter-categories'),
+};
+// GetCategories()
+
+
+// async function GetCategories(){
+try {
+  const categResult = await APLCategories(); 
+categResult.push('Show all');
+const markUpCategories = categResult.map(data => `<option value="${data}">${data}</option>`).join('');
+refs.categor.innerHTML += markUpCategories; 
+} catch (error) {
+  console.log(error);
+  }
 // }
+
+
 
 // const seacrhresult = await APLProduct('', '', '', '', '', 1);
 // console.log(seacrhresult);
