179aeb71db618efd9e628d4502a752a52cd8e1b5
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-12-24 11:37:36 +0200

    cart.html-css

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,3 +4,33 @@
 ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴
 ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴ --> */
 import { refs } from '../main';
+
+import yellowBasket from '../img/yellowBasket.png';
+import yellowBasket2x from '../img/yellowBasket2x.png' 
+
+export const cartMarkupEmpty =
+    `<div class="cart-empty-basket">
+    <picture>
+    <source class="cart-basket-img"
+     srcset="${yellowBasket} 1x, ${yellowBasket2x} 2x />
+     <img  src="${yellowBasket}"
+     class="cart-basket-img"
+     alt="yellow basket"
+     loading="lazy" />
+    </picture>
+    <div class="cart-empty-desc">
+    <ul class="cart-desc-ul">
+        <li>
+        <p>
+        Your basket is <a href="../index.html" class="cart-link">empty...</a>
+        </p>
+        </li>
+        <li>
+        <p class="cart-gray-text">
+        Go to the main page to select your favorite products and add them to the cart.
+        </p>
+        </li>
+        </ul>
+        </div>             
+`
+
