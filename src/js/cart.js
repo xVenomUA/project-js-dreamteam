69e444929bd48e2e80b54bed905a47a17b570a54
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-12-25 13:22:29 +0200

    function add-removeProducts

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,3 +4,47 @@
 ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴
 ₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴₴ --> */
 import { refs } from '../main';
+
+// // common
+// const common = {
+//     FILTERS_KEY: "filters",
+//     CART_KEY: "cart"
+// }
+
+// // localStorage
+// function saveData(data, key) {
+//     localStorage.setItem(key, JSON.stringify(data))
+// }
+
+//  function getData(key) {
+//     const data = localStorage.getItem(key);
+
+//     return data ? JSON.parse(data) : []
+// }
+
+// // addProducts
+
+// export function addProducts() {
+//     const cartArr = getData(common.CART_KEY);
+
+//     cartArr.forEach(_id => {
+//         const products = document.querySelectorAll(`[data-idcards="${_id}]`);
+
+//         products.forEach(product => {
+//             const productBtn = // знайти кнопку, яка додає товар
+//                 productBtn.innerHTML = `<svg class="_item-checked"><use href=""></></svg>`;
+//         }
+//             )
+//     })
+// }
+
+// export function removeProducts() {
+//     const cartArr = getData(common.CART_KEY);
+
+//     cartArr.forEach(_id => {
+//         const products = document.querySelectorAll(`[data-idcards="${_id}]`);
+
+
+//     }
+//     )
+// }
