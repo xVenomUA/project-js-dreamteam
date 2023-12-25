75c3dd34ba493bc460fb4bcc4b4d2b5eb7c5b7c1
Pohrebniak Andrian <117669302+xVenomUA@users.noreply.github.com>
2023-12-25 11:25:28 +0200

    Revert "commit_css,html"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // import '../node_modules/slim-select/dist/slimselect.css';
 import './js/filter';
 import './js/modal';
-// import './js/pagination';
-import './js/popular';
+import './js/popular'
 import './js/discount';
 import './js/subscription'
