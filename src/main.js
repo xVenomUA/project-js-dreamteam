30f907c1fa216d860716efc0d09283ee505c98ed
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2023-12-25 15:37:37 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // import '../node_modules/slim-select/dist/slimselect.css';
 import './js/filter';
 import './js/modal';
-import './js/popular'
+import './js/pagination';
+import './js/popular';
 import './js/discount';
 import './js/subscription'
