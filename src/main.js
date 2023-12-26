a70c02788207a0960e8102be4e3f5b9e7f493df5
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-26 14:27:47 +0200

    fi

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // import '../node_modules/slim-select/dist/slimselect.css';
+import '../node_modules/modern-normalize/modern-normalize.css';
 import './js/headerFunctionCount';
 import './js/filter';
 import './js/modal';
