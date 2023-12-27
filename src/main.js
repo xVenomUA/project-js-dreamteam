f81c219dad2b0a099059bd5789eb7e219b14bb7e
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2023-12-27 23:12:00 +0100

    com

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import '../node_modules/slim-select/dist/slimselect.css';
+import '../node_modules/slim-select/dist/slimselect.css';
 import '../node_modules/modern-normalize/modern-normalize.css';
 import './js/headerFunctionCount';
 import './js/filter';
