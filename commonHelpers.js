e4906cc4cbfcb5010806400a8eb9bea1ae63ca91
xVenomUA <117669302+xVenomUA@users.noreply.github.com>
2023-12-27 09:32:41 +0000

    Deploying to gh-pages from @ xVenomUA/project-js-dreamteam@baa86db8c4b3d6438cd443916949fef4f2f4a1e8 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{g as y,i as v}from"./assets/icon-253637f9.js";import"./assets/vendor-e21ff9e3.js";const h={FILTERS_KEY:"filters",CART_KEY:"cart"},f=document.querySelector("span#cart-counter"),b=document.querySelector(".js-cart-block"),g=document.querySelector(".cart-list-block"),S=document.querySelector(".cart-delete-btn");S.addEventListener("click",k);function k(){localStorage.removeItem("cart"),i()}function m(t){try{const e=localStorage.getItem(t);return e?JSON.parse(e):[]}catch(e){console.log(e.message)}}const E=m("cart"),l=E.map(t=>t._id);async function i(){let t=await m(h.CART_KEY);if(f.textContent=t.length,t.length===0){b.innerHTML=x();return}L()}i();async function L(){g.innerHTML="";try{let t=[],e=[];for(let c=0;c<l.length;c+=1){const r=await y(l[c]),s=w(r,l[c]);g.innerHTML+=s;const n=JSON.parse(localStorage.getItem("cart"));_(".cart-close").then(o=>{e.push(n[c]._id),console.log(e),localStorage.setItem("cart1",JSON.stringify(t)),o.forEach(u=>{u.addEventListener("click",p=>{const d=p.currentTarget.id;console.log("removed id",d),e=e.filter(a=>a!==d),console.log(e),t=[];for(let a=0;a<e.length;a+=1)t.push({_id:e[a],quantity:1});console.log(t),localStorage.setItem("cart",JSON.stringify(t)),i()})})}).catch(o=>{console.error(o.message)})}}catch(t){console.log(t)}}function _(t){return new Promise((e,c)=>{const r=document.querySelectorAll(t);if(r.length>0)e(r);else{const s=new MutationObserver(()=>{const n=document.querySelectorAll(t);n.length>0&&(s.disconnect(),e(n))});s.observe(document.documentElement,{childList:!0,subtree:!0})}})}function w(t){return t.category.includes("_")&&(t.category=t.category.replace(/_/g," ")),`
+import{g as f,i as b}from"./assets/icon-253637f9.js";import"./assets/vendor-e21ff9e3.js";const S={FILTERS_KEY:"filters",CART_KEY:"cart"},k=document.querySelector("span#cart-counter"),x=document.querySelector(".js-cart-block"),g=document.querySelector(".cart-list-block"),E=document.querySelector(".cart-delete-btn");E.addEventListener("click",C);function C(){localStorage.removeItem("cart"),d()}function u(t){try{const e=localStorage.getItem(t);return e?JSON.parse(e):[]}catch(e){console.log(e.message)}}async function d(){let t=await u(S.CART_KEY);if(k.textContent=t.length,t.length===0){x.innerHTML=w();return}L()}d();async function L(){const e=u("cart").map(c=>c._id);g.innerHTML="";try{let c=0,o=[],r=[],a=[];for(let s=0;s<e.length;s+=1){const l=await f(e[s]),m=$(l,e[s]);g.innerHTML+=m;const p=JSON.parse(localStorage.getItem("cart"));c+=l.price,o.push(l.price),console.log(c),q.textContent=`${Number(c.toFixed(2))}`,_(".cart-close").then(i=>{a.push(p[s]._id),localStorage.setItem("cart1",JSON.stringify(r)),i.forEach(y=>{y.addEventListener("click",h=>{const v=h.currentTarget.id;a=a.filter(n=>n!==v),r=[];for(let n=0;n<a.length;n+=1)r.push({_id:a[n],quantity:1});localStorage.setItem("cart",JSON.stringify(r)),d()})})}).catch(i=>{console.error(i.message)})}}catch(c){console.log(c)}}function _(t){return new Promise((e,c)=>{const o=document.querySelectorAll(t);if(o.length>0)e(o);else{const r=new MutationObserver(()=>{const a=document.querySelectorAll(t);a.length>0&&(r.disconnect(),e(a))});r.observe(document.documentElement,{childList:!0,subtree:!0})}})}function $(t){return t.category.includes("_")&&(t.category=t.category.replace(/_/g," ")),`
         <div class="cart-card-container" data-productlist-id="${t._id}">
           <div class="cart-image-container">
             <img src="${t.img}" alt="${t.name}" class="">
@@ -28,14 +28,14 @@ import{g as y,i as v}from"./assets/icon-253637f9.js";import"./assets/vendor-e21f
 
            <button type="button" id="${t._id}" class="cart-close">
               <svg class="cart-close-icon" width="18" height="18">
-                <use href="${v}#icon-close"></use>
+                <use href="${b}#icon-close"></use>
               </svg>
             </button>
 
           </div>
         </div>
       </div>
-    `}function x(){return` 
+    `}function w(){return` 
   <div class="js-cart-block">
   <div class="cart-empty-basket">
   <img class="cart-basket-img"
@@ -62,5 +62,5 @@ import{g as y,i as v}from"./assets/icon-253637f9.js";import"./assets/vendor-e21f
                 </li>
               </ul>
             </div>
-  `}
+  `}const q=document.querySelector("span#your-order-total-price");
 //# sourceMappingURL=commonHelpers.js.map
