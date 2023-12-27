cb43c83ab6e6a68d695aa6b50095bdc55e43fcff
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-27 13:57:07 +0200

    fix

--- src/js/popular.js
+++ src/js/popular.js
@@ -7,8 +7,11 @@
 import { refs } from '../js/refs';
 import { getPopularProduct, getProductById } from './APIFoodBoutique';
 import { ModalMarkUP } from './ModalMArkUp';
+import { GetCards } from './filter';
+import { onChangeCount } from './headerFunctionCount';
 import { createPopularCards } from './popularMarkup';
-  
+import iconimg from '/img/icon.svg';
+
 getRenderPopularCard(); 
 
 
@@ -41,4 +44,40 @@ async function OnClick(evt) {
     console.log(error);
   }
 }
+
+
+function OnAddCartShop(evt) {
+  GetCards();
+  const { target } = evt;
+  const parent = target.closest('.popular-icon-cont');
+  if (!parent) return;
+  const useSvg = parent.querySelector('.filt-use');
+  useSvg.setAttribute('href', `${iconimg}#icon-check`);
+  parent.setAttribute('disabled', 'true');
+  const SvgIconBack = parent.querySelector('.popular-icon ');
+  SvgIconBack.classList.add('popular-icon-back');  
+  const id = parent.dataset.idcarts;
+  const cart = localStorage.getItem('cart');
+  const parseCart = JSON.parse(cart);
+  if (parseCart) {
+    const findProduct = parseCart.find(product => product._id === id);
+    if (findProduct) {
+      findProduct.quantity += 1;
+      localStorage.setItem('cart', JSON.stringify(parseCart));
+      return;
+    }
+    parseCart.push({ _id: id, quantity: 1 });
+    localStorage.setItem('cart', JSON.stringify(parseCart));
+    onChangeCount();
+    return;
+  }
+  if (!parseCart) {
+    const cartList = [];
+    cartList.push({ _id: id, quantity: 1 });
+    localStorage.setItem('cart', JSON.stringify(cartList));
+    onChangeCount();
+  }
+}
+refs.PopularDiv.addEventListener('click', OnAddCartShop);
+
 
