f801247ef6cf93b103ff4c9a906c92eff6f8ac72
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-27 19:59:38 +0200

    f

--- src/js/popularMarkup.js
+++ src/js/popularMarkup.js
@@ -45,7 +45,7 @@ export function createPopularCards(product) {
   
             <div class="popularname-price">
           </div>
-          <button  data-idcarts="${_id}" class="popular-icon-cont">
+          <button  class="popular-buton" data-idcarts="${_id}" class="popular-icon-cont">
               <svg class="popular-icon ${backHover}" width="20" height="20">
                 <use href="${iconimg}#icon-${iconChange}" class="filt-use"></use>
               </svg>
