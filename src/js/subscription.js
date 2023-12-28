416f74be3d39acfc68eee40623840a45f0611aff
Voslo <139061915+Voslo@users.noreply.github.com>
2023-12-28 16:50:49 +0200

    Оптимізація

--- src/js/subscription.js
+++ src/js/subscription.js
@@ -3,78 +3,71 @@ import { postSubscription } from "./APIFoodBoutique";
 const form = document.querySelector('.footer-form');
 const modal = document.querySelector('.modal-backdrop');
 const modalSub = document.querySelector('.modal-subscribe-container');
-const modalUnsub= document.querySelector('.modal-unsubscription-container');
-const closeBtn = document.querySelectorAll('.modal-close-btn');
-const someModal = document.querySelector('.modal-container')
+const modalUnsub = document.querySelector('.modal-unsubscription-container');
+const closeBtns = document.querySelectorAll('.modal-close-btn');
+const loader = document.querySelector('.loader');
 const btnToTop = document.querySelector('.footer-btn-to-top');
 const containerFooter = document.querySelector('.footer-container');
-const loader = document.querySelector('.loader');
+const someModal = document.querySelector('.modal-container')
 
 someModal.classList.add('is-hidden');
-  
-const onObserve = (entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            btnToTop.style.display = 'block';
-        } else {
-            btnToTop.style.display = 'none';
-        }
-    });
-};
 
-btnToTop.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
-const observer = new IntersectionObserver(onObserve, {
-    root: null,
-    threshold: 0.5
-});
-
-observer.observe(containerFooter);
-
-
-form.addEventListener('submit', onSubmit);
-closeBtn.forEach(btn => {
-    btn.addEventListener('click', closeModal);
-});
-async function onSubmit(evt) {
-    evt.preventDefault();
-
-    const email = document.querySelector('.footer-form-email').value;
-    const data = { email };
-    loader.classList.remove('is-hidden');
-
-    try {
-        const response = await postSubscription(data);
-        loader.classList.add('is-hidden');
-        modal.classList.remove('is-hidden');
-        modalUnsub.classList.add('is-hidden');
-        modalSub.classList.remove('is-hidden');
-        console.log(response.data);
-    } catch (error) {
-        loader.classList.add('is-hidden');
-        if (error.response && error.response.status === 409) {
-            modal.classList.remove('is-hidden');
-            modalUnsub.classList.remove('is-hidden');
-            modalSub.classList.add('is-hidden');
-        } else if(error.response && error.response.status === 400) {
-            showError();
-        }
-    }
-
-    evt.target.reset();
-}
-
-function closeModal(evt) {
-    evt.preventDefault();
-    modal.classList.add('is-hidden')
+function toggleModal(isVisible, isSubscribed = true) {
+    modal.classList.toggle('is-hidden', !isVisible);
+    modalSub.classList.toggle('is-hidden', !isSubscribed);
+    modalUnsub.classList.toggle('is-hidden', isSubscribed);
 }
 
 function showError() {
     const errorMessageDiv = document.querySelector('.error-message');
     errorMessageDiv.style.display = 'block';
-    
-    setTimeout(() => {
-        errorMessageDiv.style.display = 'none';
-    }, 2000); 
+    setTimeout(() => errorMessageDiv.style.display = 'none', 2000);
 }
+
+async function onSubmit(evt) {
+    evt.preventDefault();
+    const email = form.querySelector('.footer-form-email').value;
+    loader.classList.remove('is-hidden');
+    function handleClickOutsideModal(event) {
+        if (event.target === modal) {
+            toggleModal(false);
+        }
+    }
+    modal.addEventListener('click', handleClickOutsideModal);
+
+    try {
+        const response = await postSubscription({ email });
+        console.log(response.data);
+        toggleModal(true);
+    } catch (error) {
+        if (error.response) {
+            error.response.status === 409 ? toggleModal(true, false) : showError();
+        }
+    } finally {
+        loader.classList.add('is-hidden');
+        form.reset();
+    }
+}
+
+function setupScrollToTopButton() {
+    const onObserve = (entries) => {
+        entries.forEach(entry => {
+            btnToTop.style.display = entry.isIntersecting ? 'block' : 'none';
+        });
+    };
+
+    const observer = new IntersectionObserver(onObserve, { root: null, threshold: 0.5 });
+    observer.observe(containerFooter);
+
+    btnToTop.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pathname = window.location.pathname;
+    if (pathname.endsWith("index.html") || pathname === "/") {
+        setupScrollToTopButton();
+    }
+});
+
+form.addEventListener('submit', onSubmit);
+closeBtns.forEach(btn => btn.addEventListener('click', () => toggleModal(false)));
