38366f17e630ce342cc81cdf697c2428fcbb3011
ktdrzd <137915871+ktdrzd@users.noreply.github.com>
2023-12-28 15:15:15 +0200

    iconmodal

--- src/js/discountMarkup.js
+++ src/js/discountMarkup.js
@@ -1,41 +1,50 @@
 import iconimg from '/img/icon.svg';
-import { ModalMarkUP } from './ModalMArkUp';
-
-const modalWindowId = document.getElementById('id-modal-card');
+import { refs } from './refs';
 
 export function createDiscountCards(product) {
-  const card = document.createElement("div")
-  card.classList.add("product-card")
-  card.dataset.idcarts = product._id
-  card.addEventListener("click", ()=>{
-    ModalMarkUP(product)
-    modalWindowId.classList.remove('is-hidden-card')
-  })
-  card.innerHTML = `
+  const DiscountProduct = product
+  .map(({ _id, img, name, price }) => {
+    let btnChange = '';
+    let iconChange = 'shop';
+    const localValue = JSON.parse(localStorage.getItem('cart'));
+    if (localValue) {
+      const findProduct = localValue.find(product => product._id === _id);
+      if (findProduct) {
+        iconChange = 'check';
+        btnChange = 'disabled';
+      }
+    }
+    return `
+    <div class="product-card" data-idcarts="${_id}">
   <div class="product-background">
-        
-  <div class="discount-label">
-  <svg class="discount-label-icon" width="60" height="60">
-    <use href="${iconimg}#icon-discount"></use>
-  </svg>
+    <div class="discount-label">
+      <svg class="discount-label-icon" width="60" height="60">
+        <use href="${iconimg}#icon-discount"></use>
+      </svg>
+    </div>
+
+    <img src="${img}" alt="${name}" class="product-image">
   </div>
-    <img src="${product.img}" alt="${product.name}" class="product-image">
-  </div>
+
   <div class="product-details">
-  <div class="details-text">
+    <div class="details-text">
+      <h2 class="product-name">${name}</h2>
 
-    <h2 class="product-name">${product.name}</h2>
-    
-    <div class="price-icon">
-    <p class="product-price">$${product.price.toFixed(2)}</p>
-  <div id="${product._id}" class="discount-icon-cont">
-    <svg class="discount-icon" width="18" height="18">
-      <use href="${iconimg}#icon-shop"></use>
-    </svg>
+      <div class="price-icon">
+        <p class="product-price">$${price}</p>
+
+        <button data-idcarts="${_id}" class="discount-icon-cont">
+          <svg class="discount-icon" width="18" height="18">
+            <use href="${iconimg}#icon-${iconChange}" class="filt-use"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
   </div>
-  </div>
-  </div>
-  `;
-    return card;
+</div>
+
+`;
+    })
+    .join('');
+  refs.DiscountDiv.innerHTML = DiscountProduct;
 }
-
