c6111a9fb22ceb06cf6a1a20039b80617caacb9a
Pohrebniak Andrian <pogra29@gmail.com>
2023-12-28 17:30:22 +0200

    fix

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,6 +4,10 @@ import { GetCards } from './filter';
 export const ulTag = document.querySelector('.pagination-list');
 const paginationFilt = document.querySelector('.pagination');
 const lastTOtalpage = document.querySelector('.last-total-page');
+const firstTotalPage = document.querySelector('.pag-first-page');
+const prewBtn = document.querySelector('.prew-button');
+const nextBtn = document.querySelector('.next-button');
+
 export function element(totalPages, page) {
     let liTag = '';
     let thirdPagesAnd = totalPages - 2;
@@ -52,11 +56,15 @@ export function element(totalPages, page) {
   } 
     ulTag.innerHTML = liTag;
   if (totalPages > 1 && totalPages < 7) {
-    // створи кнопки з розміткою
+    firstTotalPage.style = 'display: none';
+    lastTOtalpage.style = 'display: none';
     for (let i = 1; i <= totalPages; i++) {
       liTag += `<li class="pagination-item"><button class="pagination-number" type="button">${i}</button></li>`;
     }
-  } 
+  } else {
+    firstTotalPage.style = 'display: block';
+    lastTOtalpage.style = 'display: block';
+  }
   ulTag.innerHTML = liTag; //add li tag inside ul tag
 }
 // =============================================================================
@@ -87,12 +95,27 @@ async function prewList(event) {
 async function nextList(event) {
   const filtersParce = JSON.parse(localStorage.getItem('filters'));
   const totalP = filtersParce.totalPages;
-  filtersParce.page = filtersParce.page + 1;
+  filtersParce.page = Number(filtersParce.page)+ 1;
+  localStorage.setItem('filters', JSON.stringify(filtersParce));
+  GetCards();
+}
+
+async function firstPage(event) {
+  const filtersParce = JSON.parse(localStorage.getItem('filters'));
+  filtersParce.page = 1;
+  localStorage.setItem('filters', JSON.stringify(filtersParce));
+  GetCards();
+}
+async function lastPage(event) {
+  const filtersParce = JSON.parse(localStorage.getItem('filters'));
+  filtersParce.page = lastTOtalpage.textContent;
   localStorage.setItem('filters', JSON.stringify(filtersParce));
   GetCards();
 }
 ulTag.addEventListener('click', loadMor);
-const prewBtn = document.querySelector('.prew-button');
-const nextBtn = document.querySelector('.next-button');
+
 prewBtn.addEventListener('click', prewList);
 nextBtn.addEventListener('click', nextList);
+
+firstTotalPage.addEventListener('click', firstPage);
+lastTOtalpage.addEventListener('click', lastPage);
